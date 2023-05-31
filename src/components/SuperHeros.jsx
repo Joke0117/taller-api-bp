f62878251ea66fa6c3d8ee1cd86430838faa98bd
jose angel <martinezrodelojose@gmail.com>
2023-05-30 20:49:31 -0500

    segundo commit

---
+++ src/components/SuperHeros.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SuperHeros = () => {
+  return (
+    <div>
+        <h1>API de Super Héroes</h1>
+        <h2>José Ángel Martínez</h2>
+    </div>
+  )
+}
+
+export default SuperHeros
